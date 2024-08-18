4418f222b631956a6c52d46f3fba46881c1193d2
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-18 17:48:21 +0900

    fix: sort classnames with biome
    to resolve github actions failed in `biome check`

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -25,7 +25,7 @@ export const IconButton: FC<IconButtonProps> = ({
       iconPosition === 'right' && 'flex-row-reverse',
       'flex w-fit items-center justify-center gap-x-2 rounded-md bg-background-50 p-2 transition-colors',
       'hover:bg-background-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 focus-visible:ring-offset-background',
-      'disabled:hover:bg-background-50 disabled:text-foreground-500',
+      'disabled:text-foreground-500 disabled:hover:bg-background-50',
       props.className,
     )}
   >
