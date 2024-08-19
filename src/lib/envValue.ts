03cb5b0e8895d28d9125d7407f2ed6db1c55b6a8
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-20 07:15:31 +0900

    fix: validation error in parse `VERCEL_URL`

--- src/lib/envValue.ts
+++ src/lib/envValue.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod'
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-export const BASE_URL = isDevelopment
-  ? z.string().url().catch('http://localhost:3000').parse(process.env.BASE_URL)
-  : z.string().url().parse(process.env.VERCEL_URL)
+export const BASE_URL =
+  process.env.VERCEL_URL === undefined
+    ? z.string().url().catch('http://localhost:3000').parse(process.env.BASE_URL)
+    : z.string().url().parse(`https://${process.env.VERCEL_URL}`)
 export const POSTGRES_URL = z.string().parse(process.env.POSTGRES_URL)
 export const POSTGRES_PRISMA_URL = z.string().parse(process.env.POSTGRES_PRISMA_URL)
 export const POSTGRES_URL_NO_SSL = z.string().parse(process.env.POSTGRES_URL_NO_SSL)
