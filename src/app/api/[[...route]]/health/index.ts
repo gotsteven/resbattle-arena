8c629050c092a2a635fb8fff93c1fc2ed5492a4a
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-20 05:15:15 +0900

    feat(api): check conneting to db server with `/api/health` endpoint

--- src/app/api/[[...route]]/health/index.ts
+++ src/app/api/[[...route]]/health/index.ts
@@ -1,6 +1,11 @@
+import { sql } from '@vercel/postgres'
 import { honoFactory } from '../factory'
 
 export const healthRoute = honoFactory.createApp().get('/', async (c) => {
-  // TODO: 将来的にはDBや外部APIとの接続を確認する
-  return c.json({ status: 'ok' })
+  try {
+    await sql`SELECT CURRENT_TIMESTAMP`
+    return c.json({ status: 'ok' })
+  } catch (e) {
+    return c.json({ status: 'error' }, 500)
+  }
 })
