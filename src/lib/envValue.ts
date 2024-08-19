6f2a04fa0429e4fbb8b341e947d99bd26f737f57
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-20 03:12:00 +0900

    chore(.env): add env value of vercel/postgres

--- src/lib/envValue.ts
+++ src/lib/envValue.ts
@@ -5,3 +5,11 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 export const BASE_URL = isDevelopment
   ? z.string().url().catch('http://localhost:3000').parse(process.env.BASE_URL)
   : z.string().url().parse(process.env.VERCEL_URL)
+export const POSTGRES_URL = z.string().parse(process.env.POSTGRES_URL)
+export const POSTGRES_PRISMA_URL = z.string().parse(process.env.POSTGRES_PRISMA_URL)
+export const POSTGRES_URL_NO_SSL = z.string().parse(process.env.POSTGRES_URL_NO_SSL)
+export const POSTGRES_URL_NON_POOLING = z.string().parse(process.env.POSTGRES_URL_NON_POOLING)
+export const POSTGRES_USER = z.string().parse(process.env.POSTGRES_USER)
+export const POSTGRES_HOST = z.string().parse(process.env.POSTGRES_HOST)
+export const POSTGRES_PASSWORD = z.string().parse(process.env.POSTGRES_PASSWORD)
+export const POSTGRES_DATABASE = z.string().parse(process.env.POSTGRES_DATABASE)
