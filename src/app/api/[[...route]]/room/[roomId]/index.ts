7661d9d003826273e1adc955657de442f26e46d8
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-24 01:48:00 +0900

    fix: type error around path-param object

--- src/app/api/[[...route]]/room/[roomId]/index.ts
+++ src/app/api/[[...route]]/room/[roomId]/index.ts
@@ -5,12 +5,14 @@ import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 import { honoFactory } from '../../factory'
 
-const pathParamSchema = z.string()
+const pathParamSchema = z.object({
+  roomId: z.string(),
+})
 
 export const getRoomRoute = honoFactory
   .createApp()
   .get('/:roomId', zValidator('param', pathParamSchema), async (c) => {
-    const roomId = c.req.valid('param')
+    const { roomId } = c.req.valid('param')
     const [roomData] = await dbClient.select().from(debateRooms).where(eq(debateRooms.id, roomId))
     return c.json(roomData)
   })
