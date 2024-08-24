5d8534730d1e23ca5e2edd8e9abf54d8d978a41c
siko2525 <s1f102301820@iniad.org>
2024-08-25 07:30:17 +0900

    修正

--- src/app/api/[[...route]]/message/index.ts
+++ src/app/api/[[...route]]/message/index.ts
@@ -1,10 +1,8 @@
 import { honoFactory } from '../factory'
 import { getMessageRoute } from './get'
 import { sendMessageRoute } from './send'
-import { testRoute } from './test'
 
 export const messageRoute = honoFactory
   .createApp()
   .route('/get', getMessageRoute)
   .route('/send', sendMessageRoute)
-  .route('/test', testRoute)
