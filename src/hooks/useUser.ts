fee2142ab1c4c75669fd54ec76d42daa9a68e15a
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-25 01:14:42 +0900

    add(api): `/user` endpoint

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,24 @@
+import { apiClient } from '@/lib/apiClient'
+import type { InferRequestType } from 'hono'
+import useSWR from 'swr'
+
+export const useUser = (userId: string) => {
+  const $get = apiClient.api.user.$get
+  const fetcher = (arg: InferRequestType<typeof $get>) => async () => {
+    const res = await $get(arg)
+    return await res.json()
+  }
+
+  const { data, error, isLoading } = useSWR(
+    apiClient.api.message.get.$url().toString(),
+    fetcher({
+      query: { userId },
+    }),
+    {
+      refreshInterval: 1000,
+      revalidateOnFocus: true,
+    },
+  )
+
+  return { user: data, isError: error, isLoading }
+}
