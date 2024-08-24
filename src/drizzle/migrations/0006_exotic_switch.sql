4eade5a5b732ff832ff082d08c955ecaa7a52c2c
1F10230048 <s1f102300481@iniad.org>
2024-08-24 12:15:24 +0900

    migrationエラーを修正

--- src/drizzle/migrations/0006_exotic_switch.sql
+++
@@ -1,5 +0,0 @@
-DO $$ BEGIN
- ALTER TABLE "debate_messages" ADD CONSTRAINT "debate_messages_id_debate_rooms_id_fk" FOREIGN KEY ("id") REFERENCES "public"."debate_rooms"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
