4142d834748a2b305201d162ad5016f25d980878
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-25 01:53:55 +0900

    test

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,23 +1,22 @@
-import { auth, signIn } from '@/auth'
-import { IconButton } from '@/components/ui/IconButton'
-import { IconBrandGoogleFilled } from '@tabler/icons-react'
-import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
 
 const LogIn = async () => {
   const session = await auth()
-  if (session !== null && session !== undefined) redirect('/')
 
-  const signInAction = async () => {
-    'use server'
-    await signIn('google')
-  }
+  return <p>JSON.stringify(session)</p>
+  // if (session !== null && session !== undefined) redirect('/')
 
-  return (
-    <form action={signInAction} className="flex flex-col items-center gap-y-4">
-      <p>Googleと連携してログイン</p>
-      <IconButton type="submit" icon={IconBrandGoogleFilled} label="Googleログイン" />
-    </form>
-  )
+  // const signInAction = async () => {
+  //   'use server'
+  //   await signIn('google')
+  // }
+
+  // return (
+  //   <form action={signInAction} className="flex flex-col items-center gap-y-4">
+  //     <p>Googleと連携してログイン</p>
+  //     <IconButton type="submit" icon={IconBrandGoogleFilled} label="Googleログイン" />
+  //   </form>
+  // )
 }
 
 export default LogIn
