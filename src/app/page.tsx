80952e34437fb05e02314407f8e966f3566ff86d
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-24 16:52:44 +0900

    fix: redirect to `/auth/login`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
+import { auth } from '@/auth'
 import RoomList from '@/components/RoomList'
-import { PROJECT_NAME } from '@/constants/project'
 import { debateRooms } from '@/drizzle/schema'
 import { dbClient } from '@/lib/dbClient'
-
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 const Home = async () => {
   const allRooms = await dbClient.select().from(debateRooms)
+  const session = await auth()
+
+  if (session === null) redirect('/auth/login')
+
   return (
     <div className="flex flex-col gap-y-4 bg-background text-foreground">
-      Hello, {PROJECT_NAME}
       <RoomList rooms={allRooms} />
       <div>
         <Link href="/">部屋を更新</Link>
       </div>
       <Link href="/room/create">部屋を作る</Link>
-      <Link href="/auth/login">ログインする</Link>
     </div>
   )
 }
