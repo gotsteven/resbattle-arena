a2cadf230ca3c480a9794d4f3558f45242d44d22
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-25 14:37:05 +0900

    fix: バトル結果の表示のデザインを修正

--- src/app/room/[roomId]/room-result.tsx
+++ src/app/room/[roomId]/room-result.tsx
@@ -1,25 +1,72 @@
+import { Loading } from '@/components/ui/Loading'
 import { apiClient } from '@/lib/apiClient'
 import type { Result, Room } from '@/types/types'
-import { useEffect, useState } from 'react'
+import { IconSkull, IconTrophy } from '@tabler/icons-react'
+import { type FC, useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-const RoomResult = ({ room }: { room: Room }) => {
+type RoomResultProps = {
+  room: Room
+  userId: string
+}
+
+const RoomResult: FC<RoomResultProps> = ({ room, userId }) => {
   const [result, setResult] = useState<Result>()
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
     const fetchResult = async () => {
       const res = await apiClient.api.result.$get({ query: { roomId: room.id } })
       const result = await res.json()
       setResult(result)
     }
-    fetchResult()
+    fetchResult().then(() => setIsLoading(false))
   }, [room.id])
+
+  if (isLoading || result === undefined) return <Loading />
+
+  const winner = result.ad_p1 > result.ad_p2 ? room.player1_id : room.player2_id
+
   return (
-    <div>
-      <div>勝者: {result?.winner === 1 ? '賛成派' : '反対派'}</div>
-      <div>優勢率</div>
-      <p>Player1: {result?.ad_p1}</p>
-      <p>Player2: {result?.ad_p2}</p>
-      <div>判定の根拠: {result?.reason}</div>
-      <div>フィードバック: {result?.feedback}</div>
+    <div className="flex flex-col items-center gap-y-8">
+      <div className="flex flex-col items-center gap-y-4">
+        {winner === userId ? (
+          <IconTrophy size={48} className="text-orange-400" />
+        ) : (
+          <IconSkull size={48} className="text-gray-600" />
+        )}
+        <p className="font-bold text-lg">{winner === userId ? 'YOU WIN!!!' : 'YOU LOSE...'}</p>
+      </div>
+      <div className="w-full">
+        <div className="flex justify-between">
+          <span>{result.ad_p1}%</span>
+          <span> {result.ad_p2}%</span>
+        </div>
+        <div className="flex w-full overflow-hidden rounded-full">
+          <span
+            style={{ width: `${result.ad_p1}%` }}
+            className={twMerge(
+              'block h-3 bg-background-50',
+              room.player1_id === userId && 'bg-accent/60',
+            )}
+          />
+          <span
+            style={{ width: `${result.ad_p2}%` }}
+            className={twMerge(
+              'block h-3 bg-background-50',
+              room.player2_id === userId && 'bg-accent/60',
+            )}
+          />
+        </div>
+      </div>
+      <div>
+        <h3 className="text-center font-bold text-lg">判定の根拠</h3>
+        {result.reason}
+      </div>
+      <div>
+        <h3 className="text-center font-bold text-lg">フィードバック</h3>
+        {result.feedback}
+      </div>
     </div>
   )
 }
