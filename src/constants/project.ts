e63656c0642b468e2a63afc42212247e66c30e35
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-05 23:19:03 +0900

    remove: `jun-eg`

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,6 +1,6 @@
 export const PROJECT_ID = 'resbattle-arena'
 export const PROJECT_NAME = 'ResBattleArena'
-export const PROJECT_AUTHOR = ['1F10230048', 'mst-mkt', 'skmtrd', 'jun-eg', 'siko2525'] as const
+export const PROJECT_AUTHOR = ['1F10230048', 'mst-mkt', 'skmtrd', 'siko2525'] as const
 export const PROJECT_OWNER = '1F10230048'
 export const PROJECT_REPOSITORY_URL = `https://github.com/${PROJECT_OWNER}/${PROJECT_ID}` as const
 export const PROJECT_LINKS = [
