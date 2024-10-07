5e673d5d8d75b7b87ee1576af01f542eae85fce0
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-07 16:46:50 +0900

    add: mistral ai model

--- src/lib/aiModels.ts
+++ src/lib/aiModels.ts
@@ -1,7 +1,8 @@
 import { createAnthropic } from '@ai-sdk/anthropic'
 import { createGoogleGenerativeAI } from '@ai-sdk/google'
+import { createMistral } from '@ai-sdk/mistral'
 import { createOpenAI } from '@ai-sdk/openai'
-import { CLAUDE_API_KEY, GEMINI_API_KEY, OPENAI_API_KEY } from './envValue'
+import { CLAUDE_API_KEY, GEMINI_API_KEY, MISTRAL_API_KEY, OPENAI_API_KEY } from './envValue'
 
 const openaiProvider = createOpenAI({
   apiKey: OPENAI_API_KEY,
@@ -22,15 +23,22 @@ const googleAiProvider = createGoogleGenerativeAI({
 
 export const googleAiModel = googleAiProvider('gemini-1.5-pro')
 
-export const aiModels = [openaiModel, anthropicModel, googleAiModel] as const
+const mistralProvider = createMistral({
+  apiKey: MISTRAL_API_KEY,
+})
+
+export const mistralModel = mistralProvider('mistral-large-latest')
+
+export const aiModels = [openaiModel, anthropicModel, googleAiModel, mistralModel] as const
 export type AiModel = (typeof aiModels)[number]
 
-const aiModelNames = ['gpt', 'claude', 'gemini'] as const
+const aiModelNames = ['gpt', 'claude', 'gemini', 'mistral'] as const
 type AiModelNames = (typeof aiModelNames)[number]
 
 export const getAiModelName = (model: string): AiModelNames => {
   if (model === openaiModel.provider) return 'gpt'
   if (model === anthropicModel.provider) return 'claude'
   if (model === googleAiModel.provider) return 'gemini'
+  if (model === mistralModel.provider) return 'mistral'
   return 'gpt' // default value
 }
